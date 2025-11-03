62119994e7f55e9539b41aa9fc809a4d98948ea7
folladorgabriel <gabrielrorigfollador@gmail.com>
2025-11-03 08:43:18 -0300

    Update TrabalhoClinicaMedica.sql

--- TrabalhoClinicaMedica.sql
+++ TrabalhoClinicaMedica.sql
@@ -227,38 +227,40 @@ insert into Exame(Data_Exame, Nome, Diagnóstico) values
 ('2025-02-27', 'Teste de Covid-19', 'Negativo');
 
 insert into Médico (Nome, Crm, Telefone, Email, Cpf, idEspecialidade) values
-('Dr. João Silva', 'CRM12345SP', '(11) 91234-5678', 'joao.silva@clinicavida.com', '123.456.789-00', 'Cardiologista'), /* Botar especilidade do medico*/
-('Dra. Maria Oliveira', 'CRM67890RJ', '(21) 99876-5432', 'maria.oliveira@saudebem.com', '234.567.890-11'),
-('Dr. Pedro Santos', 'CRM11223MG', '(31) 98765-4321', 'pedro.santos@hospitalcentral.com', '345.678.901-22'),
-('Dra. Ana Costa', 'CRM33445RS', '(51) 92345-6789', 'ana.costa@clinicageral.com', '456.789.012-33'),
-('Dr. Lucas Pereira', 'CRM55667BA', '(71) 98877-6655', 'lucas.pereira@coracaoemdia.com', '567.890.123-44'),
-('Dra. Carla Mendes', 'CRM77889PR', '(41) 99788-5544', 'carla.mendes@vidasaude.com', '678.901.234-55'),
-('Dr. Rafael Lima', 'CRM99001PE', '(81) 91234-4433', 'rafael.lima@neurocenter.com', '789.012.345-66'),
-('Dra. Beatriz Rocha', 'CRM22334SC', '(48) 99654-3322', 'beatriz.rocha@pediatriabrasil.com', '890.123.456-77'),
-('Dr. Gustavo Martins', 'CRM88990RS', '(51) 98123-1111', 'gustavo.martins@clinicars.com', '101.202.303-44'),
-('Dra. Helena Souza', 'CRM99112SP', '(11) 93456-2222', 'helena.souza@saudesp.com', '202.303.404-55'),
-('Dr. André Carvalho', 'CRM11223RJ', '(21) 94567-3333', 'andre.carvalho@hospitalrj.com', '303.404.505-66'),
-('Dra. Patrícia Gomes', 'CRM22334MG', '(31) 97654-4444', 'patricia.gomes@clinicabh.com', '404.505.606-77'),
-('Dr. Felipe Costa', 'CRM33445BA', '(71) 99876-5555', 'felipe.costa@bahiasaude.com', '505.606.707-88'),
-('Dra. Larissa Almeida', 'CRM44556PE', '(81) 98765-6666', 'larissa.almeida@neuroclinic.com', '606.707.808-99'),
-('Dr. Thiago Ribeiro', 'CRM55667SC', '(48) 95678-7777', 'thiago.ribeiro@medfloripa.com', '707.808.909-10'),
-('Dra. Amanda Farias', 'CRM66778PR', '(41) 95432-8888', 'amanda.farias@vidaparnaiba.com', '808.909.010-11');
+('Dr. João Silva', 'CRM12345SP', '(11) 91234-5678', 'joao.silva@clinicavida.com', '123.456.789-00', '1'), /* Botar especilidade do medico*/
+('Dra. Maria Oliveira', 'CRM67890RJ', '(21) 99876-5432', 'maria.oliveira@saudebem.com', '234.567.890-11', '0'),
+('Dr. Pedro Santos', 'CRM11223MG', '(31) 98765-4321', 'pedro.santos@hospitalcentral.com', '345.678.901-22', '3'),
+('Dra. Ana Costa', 'CRM33445RS', '(51) 92345-6789', 'ana.costa@clinicageral.com', '456.789.012-33', '2'),
+('Dr. Lucas Pereira', 'CRM55667BA', '(71) 98877-6655', 'lucas.pereira@coracaoemdia.com', '567.890.123-44', '6'),
+('Dra. Carla Mendes', 'CRM77889PR', '(41) 99788-5544', 'carla.mendes@vidasaude.com', '678.901.234-55', '4'),
+('Dr. Rafael Lima', 'CRM99001PE', '(81) 91234-4433', 'rafael.lima@neurocenter.com', '789.012.345-66', '5'),
+('Dra. Beatriz Rocha', 'CRM22334SC', '(48) 99654-3322', 'beatriz.rocha@pediatriabrasil.com', '890.123.456-77', '8'),
+('Dr. Gustavo Martins', 'CRM88990RS', '(51) 98123-1111', 'gustavo.martins@clinicars.com', '101.202.303-44', '7' ),
+('Dra. Helena Souza', 'CRM99112SP', '(11) 93456-2222', 'helena.souza@saudesp.com', '202.303.404-55', '10'),
+('Dr. André Carvalho', 'CRM11223RJ', '(21) 94567-3333', 'andre.carvalho@hospitalrj.com', '303.404.505-66', '9'),
+('Dra. Patrícia Gomes', 'CRM22334MG', '(31) 97654-4444', 'patricia.gomes@clinicabh.com', '404.505.606-77', '13'),
+('Dr. Felipe Costa', 'CRM33445BA', '(71) 99876-5555', 'felipe.costa@bahiasaude.com', '505.606.707-88', '12'),
+('Dra. Larissa Almeida', 'CRM44556PE', '(81) 98765-6666', 'larissa.almeida@neuroclinic.com', '606.707.808-99', '11'),
+('Dr. Thiago Ribeiro', 'CRM55667SC', '(48) 95678-7777', 'thiago.ribeiro@medfloripa.com', '707.808.909-10', '15'),
+('Dra. Amanda Farias', 'CRM66778PR', '(41) 95432-8888', 'amanda.farias@vidaparnaiba.com', '808.909.010-11' ,'14');
 
 insert into Paciente (Nome, Cpf, Data_Nascimento, Sexo, Telefone, Email, Endereço, Bairro, idConvênio, idCidade) values 
-('João Pedro da Silva', '123.456.789-00', '1990-03-15', 'M', '(11) 91234-5678', 'joao.silva@email.com', 'Rua das Flores, 123', 'Centro', 'Unimed', 'Erechim'), /*Adiciona o convênio e cidade, tem um exemplo nessa linha*/
-('Maria Fernanda Alves', '234.567.890-11', '1985-07-22', 'F', '(21) 99876-5432', 'maria.alves@email.com', 'Av. Atlântica, 456', 'Copacabana'),
-('Carlos Eduardo Lima', '345.678.901-22', '1978-12-05', 'M', '(31) 98765-4321', 'carlos.lima@email.com', 'Rua dos Andradas, 789', 'Savassi'),
-('Ana Paula Rocha', '456.789.012-33', '1992-09-18', 'F', '(51) 92345-6789', 'ana.rocha@email.com', 'Rua Bento Gonçalves, 55', 'Cidade Baixa'),
-('Lucas Henrique Souza', '567.890.123-44', '1995-02-10', 'M', '(71) 98877-6655', 'lucas.souza@email.com', 'Rua Bahia, 890', 'Pituba'),
-('Beatriz Mendes Costa', '678.901.234-55', '1988-05-30', 'F', '(41) 99788-5544', 'beatriz.costa@email.com', 'Rua XV de Novembro, 300', 'Batel'),
-('Rafael Tavares Moreira', '789.012.345-66', '1980-10-12', 'M', '(81) 91234-4433', 'rafael.moreira@email.com', 'Rua Aurora, 210', 'Boa Vista'),
-('Juliana Pereira Lima', '890.123.456-77', '1993-01-25', 'F', '(48) 99654-3322', 'juliana.lima@email.com', 'Rua das Acácias, 45', 'Trindade'),
-('Fernando Carvalho Nunes', '901.234.567-88', '1986-06-09', 'M', '(62) 98543-2211', 'fernando.nunes@email.com', 'Rua Goiás, 12', 'Setor Oeste'),
-('Camila Ribeiro Duarte', '012.345.678-99', '1998-11-03', 'F', '(85) 99911-2200', 'camila.duarte@email.com', 'Av. Beira Mar, 789', 'Meireles'),
-('Pedro Henrique Ramos', '111.222.333-44', '1991-04-12', 'M', '(11) 97777-1111', 'pedro.ramos@email.com', 'Rua Aroeiras, 20', 'Jardim Paulista'),
-('Larissa Nogueira Lopes', '222.333.444-55', '1987-09-09', 'F', '(21) 98888-2222', 'larissa.lopes@email.com', 'Av. Copacabana, 120', 'Ipanema'),
-('Ricardo Lima Santos', '333.444.555-66', '1979-01-28', 'M', '(31) 97777-3333', 'ricardo.santos@email.com', 'Rua Ouro Preto, 500', 'Savassi'),
-('Juliana Costa Pereira', '444.555.666-77', '1994-06-17', 'F', '(51) 95678-4444', 'juliana.pereira@email.com', 'Rua Osório, 80', 'Moinhos de Vento');
+('João Pedro da Silva', '123.456.789-00', '1990-03-15', 'M', '(11) 91234-5678', 'joao.silva@email.com', 'Rua das Flores, 123', 'Centro', '2', '1'), /*Adiciona o convênio e cidade, tem um exemplo nessa linha*/
+('Maria Fernanda Alves', '234.567.890-11', '1985-07-22', 'F', '(21) 99876-5432', 'maria.alves@email.com', 'Av. Atlântica, 456', 'Copacabana', '1', '0'),
+('Carlos Eduardo Lima', '345.678.901-22', '1978-12-05', 'M', '(31) 98765-4321', 'carlos.lima@email.com', 'Rua dos Andradas, 789', 'Savassi', '0', '0'),
+('Ana Paula Rocha', '456.789.012-33', '1992-09-18', 'F', '(51) 92345-6789', 'ana.rocha@email.com', 'Rua Bento Gonçalves, 55', 'Cidade Baixa', '3', '10'),
+('Lucas Henrique Souza', '567.890.123-44', '1995-02-10', 'M', '(71) 98877-6655', 'lucas.souza@email.com', 'Rua Bahia, 890', 'Pituba', '4', '11'),
+('Beatriz Mendes Costa', '678.901.234-55', '1988-05-30', 'F', '(41) 99788-5544', 'beatriz.costa@email.com', 'Rua XV de Novembro, 300', 'Batel', '5', '4'),
+('Rafael Tavares Moreira', '789.012.345-66', '1980-10-12', 'M', '(81) 91234-4433', 'rafael.moreira@email.com', 'Rua Aurora, 210', 'Boa Vista','6','3'),
+('Juliana Pereira Lima', '890.123.456-77', '1993-01-25', 'F', '(48) 99654-3322', 'juliana.lima@email.com', 'Rua das Acácias, 45', 'Trindade', '7', '2'),
+('Fernando Carvalho Nunes', '901.234.567-88', '1986-06-09', 'M', '(62) 98543-2211', 'fernando.nunes@email.com', 'Rua Goiás, 12', 'Setor Oeste','8', '6'),
+('Camila Ribeiro Duarte', '012.345.678-99', '1998-11-03', 'F', '(85) 99911-2200', 'camila.duarte@email.com', 'Av. Beira Mar, 789', 'Meireles','9', '5'),
+('Pedro Henrique Ramos', '111.222.333-44', '1991-04-12', 'M', '(11) 97777-1111', 'pedro.ramos@email.com', 'Rua Aroeiras, 20', 'Jardim Paulista','10', '7'),
+('Larissa Nogueira Lopes', '222.333.444-55', '1987-09-09', 'F', '(21) 98888-2222', 'larissa.lopes@email.com', 'Av. Copacabana, 120', 'Ipanema', '10', '8'),
+('Ricardo Lima Santos', '333.444.555-66', '1979-01-28', 'M', '(31) 97777-3333', 'ricardo.santos@email.com', 'Rua Ouro Preto, 500', 'Savassi','8','7'),
+('Juliana Costa Pereira', '444.555.666-77', '1994-06-17', 'F', '(51) 95678-4444', 'juliana.pereira@email.com', 'Rua Osório, 80', 'Moinhos de Vento', '0', '4');
+
+select * from Paciente;
 
 /*Criar 3 UPDATES em quaisquer das tabelas..
 Criar 3 UPDATES com Where em quaisquer das tabelas..
@@ -367,7 +369,7 @@ SELECT Nome, Crm, Telefone, Email FROM Médico ORDER BY Crm ASC;
 SELECT Data_Exame, Nome, Diagnóstico FROM Exame ORDER BY Data_Exame ASC;
 SELECT Nome, UF, Regiao FROM Cidade ORDER BY Regiao ASC, Nome ASC;
 
-/* 5x Inner join 
+/* 5x Inner join */
 
 SELECT exame.Data_Exame, consulta.Data_Consulta
 FROM Exame
